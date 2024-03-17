427b71c0bb64f02640421b4cda7327b02c776142
goiabae <goiabae@protonmail.com>
2024-03-17 17:25:30 -0300

    improve test script
    Output test headers with colors. Don't print out commands as they are
    being executed.

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 
-set -eux
+set -eu
+
+purple='\e[0;35m'
+reset='\e[0;30m'
 
 for f in ./examples/*.fala; do
 	f="${f##*/}"
 	f="${f%.*}"
 
-	echo "Testing $f"
+	echo -e "\n${purple}TESTING ${f}${reset}"
 	if [ -f ./examples/$f.input ]; then
 		./build/fala ./examples/$f.fala < ./examples/$f.input
 	else
