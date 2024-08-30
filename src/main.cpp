24eaf5a929cca1e49a92542513174910b4de727d
goiabae <gm4ch4d0@gmail.com>
2024-08-30 12:28:48 -0300

    properly parse options on windows

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,6 @@
 
 #include "vm.hpp"
 
-#ifndef _WIN32
-#	include <getopt.h>
-#endif
-
 // clang-format off
 // need to be included in this order
 extern "C" {
@@ -65,11 +61,58 @@ static void usage() {
 	);
 }
 
+// getopt comes from POSIX which is not available on Windows
+#ifdef _WIN32
+int optind = 1;
+char* optarg = nullptr;
+
+bool is_option(const char* str) { return strlen(str) == 2 && str[0] == '-'; }
+
+int getopt(int argc, char** argv, const char* opts_) {
+	static const char* opts = opts_;
+
+	for (size_t i = 0; i < strlen(opts); i++) {
+		if (opts[i] == ':') continue;
+
+		// iterate the remaining arguments
+		for (int j = optind; j < argc; j++) {
+			int window_len = (is_option(argv[j]) && argv[j][1] == opts[i]) ? 2 : 1;
+
+			// option found in argument vector
+			if (is_option(argv[j]) && argv[j][1] == opts[i]) {
+				if (opts[i + 1] == ':' && !is_option(argv[j + 1])) {
+					char* opt = argv[j];
+					char* arg = argv[j + 1];
+
+					for (int k = j + 1; k >= optind + 1; k--) argv[k] = argv[k - 1 - 1];
+
+					argv[optind] = opt;
+					argv[optind + 1] = arg;
+				} else {
+					char* opt = argv[j];
+
+					for (int k = j + 0; k >= optind + 0; k--) argv[k] = argv[k - 1];
+
+					argv[optind] = opt;
+				}
+
+				optind += window_len;
+				optarg = (window_len > 1) ? argv[j + 1] : nullptr;
+
+				return opts[i];
+			}
+		}
+	}
+
+	return -1;
+}
+#else
+#	include <getopt.h>
+#endif
+
 Options parse_args(int argc, char* argv[]) {
 	Options opts {};
 
-	// getopt comes from POSIX which is not available on Windows
-#ifndef _WIN32
 	for (char c = 0; (c = (char)getopt(argc, argv, "Vwo:ci")) != -1;) switch (c) {
 			case 'V': opts.verbose = true; break;
 			case 'w': opts.use_walk_interpreter = true; break;
@@ -78,24 +121,7 @@ Options parse_args(int argc, char* argv[]) {
 			case 'i': opts.interpret = true; break;
 			default: break;
 		}
-#else
-	size_t optind = 1;
 
-	for (size_t i = 1; i < argc; i++) {
-		if (argv[i][0] == '-' && argv[i][1] != '\0') {
-			switch (argv[i][1]) {
-				// FIXME parser -o output file
-				case 'V': opts.verbose = true; break;
-				case 'w': opts.use_walk_interpreter = true; break;
-				case 'c': opts.compile = true; break;
-				case 'i': opts.interpret = true; break;
-			}
-			optind++;
-		} else
-			break;
-	}
-
-#endif
 	opts.argv = &argv[optind];
 	opts.argc = argc - optind;
 
