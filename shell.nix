4dc1345fa3d7aa3ed51b7ad6707597b0f643fc6a
goiabae <gm4ch4d0@gmail.com>
2024-10-23 23:49:12 -0300

    use newest bison version

--- shell.nix
+++ shell.nix
@@ -1,13 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
-let
-  rev = "0bcbb978795bab0f1a45accc211b8b0e349f1cdb";
-  old = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  }) {};
-in with pkgs; mkShell {
+with pkgs; mkShell {
   nativeBuildInputs = [
     stdenv.cc
-    old.bison # 3.0.5
+    bison
     readline
     cmake
     cppcheck
