1e50cc43812ba6e3ef9b11f7d1f8b075c0cfeb6c
goiabae <gm4ch4d0@gmail.com>
2025-01-08 21:53:08 -0300

    document weird nix behavior with clangd

--- shell.nix
+++ shell.nix
@@ -7,7 +7,13 @@ with pkgs; mkShell {
     cmake
     cppcheck
     clang-analyzer
+
+    # The clangd executable provided by the "clang" package uses the unwrapped
+    # version of the clang executable, which has no idea where to find the
+    # standard library headers and will just report them as missing. Instead,
+    # always use the clangd from "clang-tools"
     clang-tools
+
     gdb
   ];
 
