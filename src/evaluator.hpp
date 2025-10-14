15d02dfd6960363d4ac574f064840297f9882d90
goiabae <gm4ch4d0@gmail.com>
2025-10-13 21:53:20 -0300

    evaluator: threading context by value was way too slow

--- src/evaluator.hpp
+++ src/evaluator.hpp
@@ -4,7 +4,6 @@
 #include <concepts>
 #include <optional>
 #include <span>
-#include <utility>
 
 #include "ast.hpp"
 #include "str_pool.h"
@@ -18,38 +17,41 @@ concept evaluator = requires(
 	std::optional<NodeIndex> type_idx, std::span<NodeIndex> param_idxs,
 	NodeIndex var_idx, StrID string_id, NodeIndex index_idx, int integer,
 	StrID effect_id, NodeIndex left_idx, NodeIndex right_idx, char operation,
-	bool boolean, char character, NodeIndex generic_idx
+	bool boolean, char character, NodeIndex generic_idx,
+	std::span<NodeIndex> exp_idxs
 ) {
 	// clang-format off
-	{ ev.eval_application(ctx, func_idx, arg_idxs) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_if_expression(ctx, cond_idx, then_idx, else_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_for_loop(ctx, decl_idx, upto_idx, step_idx, then_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_while_loop(ctx, cond_idx, then_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_let_binding(ctx, decl_idxs, exp_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_variable_declaration(ctx, id_idx, type_idx, exp_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_function_declaration(ctx, id_idx, param_idxs, type_idx, exp_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_assignment(ctx, var_idx, exp_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_string(ctx, string_id) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_indexing(ctx, var_idx, index_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_integer(ctx, integer) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_break(ctx, exp_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_continue(ctx, exp_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_arithmetic(ctx, operation, left_idx, right_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_logical(ctx, operation, left_idx, right_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_negation(ctx, exp_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_comparison(ctx, operation, left_idx, right_idx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_variable(ctx, string_id) } -> std::same_as<std::optional<Value&>>;
-	{ ev.eval_boolean(ctx, boolean) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_nil(ctx) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_character(ctx, character) } -> std::same_as<std::pair<Context, Value>>;
-	{ ev.eval_type_instance(ctx, generic_idx, arg_idxs) } -> std::same_as<std::pair<Context, Type>>;
-	{ ev.eval_type_variable(ctx, string_id) } -> std::same_as<std::pair<Context, Type>>;
+	{ ev.eval_application(func_idx, arg_idxs) } -> std::same_as<Value>;
+	{ ev.eval_if_expression(cond_idx, then_idx, else_idx) } -> std::same_as<Value>;
+	{ ev.eval_for_loop(decl_idx, upto_idx, step_idx, then_idx) } -> std::same_as<Value>;
+	{ ev.eval_while_loop(cond_idx, then_idx) } -> std::same_as<Value>;
+	{ ev.eval_let_binding(decl_idxs, exp_idx) } -> std::same_as<Value>;
+	{ ev.eval_variable_declaration(id_idx, type_idx, exp_idx) } -> std::same_as<Value>;
+	{ ev.eval_function_declaration(id_idx, param_idxs, type_idx, exp_idx) } -> std::same_as<Value>;
+	{ ev.eval_assignment(var_idx, exp_idx) } -> std::same_as<Value>;
+	{ ev.eval_string(string_id) } -> std::same_as<Value>;
+	{ ev.eval_indexing(var_idx, index_idx) } -> std::same_as<Value>;
+	{ ev.eval_integer(integer) } -> std::same_as<Value>;
+	{ ev.eval_break(exp_idx) } -> std::same_as<Value>;
+	{ ev.eval_continue(exp_idx) } -> std::same_as<Value>;
+	{ ev.eval_arithmetic(operation, left_idx, right_idx) } -> std::same_as<Value>;
+	{ ev.eval_logical(operation, left_idx, right_idx) } -> std::same_as<Value>;
+	{ ev.eval_negation(exp_idx) } -> std::same_as<Value>;
+	{ ev.eval_comparison(operation, left_idx, right_idx) } -> std::same_as<Value>;
+	{ ev.eval_variable(string_id) } -> std::same_as<std::optional<std::reference_wrapper<Value>>>;
+	{ ev.eval_boolean(boolean) } -> std::same_as<Value>;
+	{ ev.eval_nil() } -> std::same_as<Value>;
+	{ ev.eval_character(character) } -> std::same_as<Value>;
+	{ ev.eval_type_instance(generic_idx, arg_idxs) } -> std::same_as<Type>;
+	{ ev.eval_type_variable(string_id) } -> std::same_as<Type>;
+	{ ev.eval_block(exp_idxs) } -> std::same_as<Value>;
 
-	// TODO: right now you cannot resume to the point of throw after handling
-	{ ctx.find_variable(string_id) } -> std::same_as<std::optional<Value&>>;
-	{ ctx.find_type_variable(string_id) } -> std::same_as<std::optional<Value&>>;
-	{ ctx.yielding_effect() } -> std::same_as<std::optional<StrID>>;
-	{ ctx.can_handle(string_id) } -> std::same_as<bool>;
+	{ ev.find_variable(string_id) } -> std::same_as<std::optional<std::reference_wrapper<Value>>>;
+	{ ev.find_type_variable(string_id) } -> std::same_as<std::optional<std::reference_wrapper<Type>>>;
+	{ ev.yielding_effect() } -> std::same_as<std::optional<StrID>>;
+	{ ev.can_handle(string_id) } -> std::same_as<bool>;
+	{ ev.enter_new_scope() } -> std::same_as<void>;
+	{ ev.close_current_scope() } -> std::same_as<void>;
 	// clang-format on
 };
 
