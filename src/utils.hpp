0c2f264de74c3a0d25f391e6eafdf9df7ea423a4
goiabae <gm4ch4d0@gmail.com>
2026-01-06 13:04:43 -0300

    poor mans pattern matching

--- src/utils.hpp
+++ src/utils.hpp
@@ -40,4 +40,28 @@ std::ostream& operator<<(
 	return st;
 }
 
+template<class... Ts>
+struct overloaded : Ts... {
+	using Ts::operator()...;
+};
+
+#define PARENS ()
+
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+
+#define EACH(M, ...) __VA_OPT__(EXPAND(EACH_AUX(M, __VA_ARGS__)))
+#define EACH_AUX(M, X, ...) \
+	M(X) __VA_OPT__(, ) __VA_OPT__(EACH_AGAIN PARENS(M, __VA_ARGS__))
+#define EACH_AGAIN() EACH_AUX
+
+#define ADD_REF(X) [&] X
+
+#define MATCH(X, ...) std::visit(overloaded {EACH(ADD_REF, __VA_ARGS__)}, X)
+#define TRY_MATCH(X, ...) \
+	MATCH(X, __VA_ARGS__, (const auto& _) { assert(false); })
+
 #endif
